725fd125bc295551994b9118c50224d3745044ce
amazingshellyyy <amazingshellyyy@gmail.com>
2020-04-11 16:25:57 -0700

    unify function for getting data

--- getData/getData.js
+++ getData/getData.js
@@ -7,68 +7,70 @@ const cp = require('child_process')
 
 
 const getCountyData = (stateCode, url, selector, countyIndex, caseIndex, deathIndex ) => {
-    if(stateCode =='NY') {
-        const a = 1;
-        const a = 2;
-    }
-    const covidHistory = require(`../docs/US-${stateCode}/countyTimeseries.json`);   
-    let covidData = covidHistory;
-    // console.log(covidData);
-    console.log(url)
-    request({
-        method: 'GET',
-        url: url
-    }, (err, res, body) => {
-        if (err) return console.error(err);
-        console.log('res', res && res.statusCode)
-        if (res.statusCode == 200) {
-            let data = []
-            let $ = cheerio.load(body);
-            $.fn.ignore = function (sel) {
-                return this.clone().find(sel || ">*").remove().end();
-            };
-            const rows = $(`${selector}`)
-            if (!Array.isArray(rows)) {
-                return console.log('selector is wrong')
-            }
-            rows.each((index, el) => {
-                if (index < rows.length && index > 1) {
-                    data.push($(el).ignore("sup").text());
-                }
-            })
-            // console.log('data',data)
-            let newData = [];
-            data.forEach(county => {
-                const datastr = JSON.stringify(county).split('\\n');
-                // console.log(datastr);
-                if (datastr[countyIndex] && parseInt(datastr[caseIndex])) {
-                    const datas = {
-                        "county": datastr[countyIndex],
-                        "case": parseInt(datastr[caseIndex].split(',').join('')),
-                        "death": parseInt(datastr[deathIndex].split(',').join(''))
+    return new Promise((resolve, reject)=>{
+        try {
+            const covidHistory = require(`../docs/US-${stateCode}/countyTimeseries.json`);   
+            let covidData = covidHistory;
+            // console.log(covidData);
+            // console.log(url)
+            request({
+                method: 'GET',
+                url: url
+            }, (err, res, body) => {
+                if (err) return reject(err);
+                console.log('res', res && res.statusCode)
+                // console.log(selector);
+                if (res.statusCode == 200) {
+                    let data = []
+                    let $ = cheerio.load(body);
+                    $.fn.ignore = function (sel) {
+                        return this.clone().find(sel || ">*").remove().end();
+                    };
+                    const rows = $(selector);
+                    
+                    rows.each((index, el) => {
+                        if (index < rows.length && index > 1) {
+                            data.push($(el).ignore("sup").text());
+                        }
+                    })
+                    // console.log('data',data)
+                    let newData = [];
+                    data.forEach(county => {
+                        const datastr = JSON.stringify(county).split('\\n');
+                        // console.log(datastr);
+                        if (datastr[countyIndex] && parseInt(datastr[caseIndex])) {
+                            const datas = {
+                                "county": datastr[countyIndex],
+                                "case": parseInt(datastr[caseIndex].split(',').join('')),
+                                "death": parseInt(datastr[deathIndex].split(',').join(''))
+                            }
+                            newData.push(datas);
+                        }
+                    })
+        
+                    let timeseriesData = {
+                        timeStamp: getCurrentTime(),
+                        data: newData
                     }
-                    newData.push(datas);
-                }
-            })
-
-            let timeseriesData = {
-                timeStamp: getCurrentTime(),
-                data: newData
-            }
-            covidData.push(timeseriesData);
-            covidData.sort((a, b) => (a.timeStamp > b.timeStamp) ? 1 : -1)
-            fs.writeFile(path.join(__dirname, `../docs/US-${stateCode}/countyTimeseries.json`), JSON.stringify(covidData, null, 2), function (err) {
-                if (err) {
-                    console.log(err);
+                    covidData.push(timeseriesData);
+                    covidData.sort((a, b) => (a.timeStamp > b.timeStamp) ? 1 : -1)
+                    fs.writeFile(path.join(__dirname, `../docs/US-${stateCode}/countyTimeseries.json`), JSON.stringify(covidData, null, 2), function (err) {
+                        if (err) {
+                            return reject(err);
+                        } else {
+                            return resolve(`finish writing ${stateCode} file`)
+                        }
+        
+                    })
                 } else {
-                    console.log(`finish writing ${stateCode} file`)
+                    return reject(res.statusCode);
                 }
-
-            })
-        } else {
-            console.log('failed', res.statusCode)
+            });
+        } catch (err) {
+            return reject(err);
         }
-    });
+
+    })
 }
 
 
@@ -77,7 +79,18 @@ const getCountyData = (stateCode, url, selector, countyIndex, caseIndex, deathIn
 // getCountyData('NY', 'https://en.wikipedia.org/wiki/2020_coronavirus_pandemic_in_New_York_(state)', '.sortable tbody tr[class!=sortbottom]', 1,3,5);
 // getCountyData('CA', 'https://en.wikipedia.org/wiki/2020_coronavirus_pandemic_in_California', '.sortable tbody tr[class!=sortbottom]', 1,3,5);
 
+const successCallback = (result) =>{
+    console.log(result);
+}
+
+const failureCallback = (error) => {
+    console.error("Error:"+error);
+}
+
+
 
 module.exports = {
-    getCountyData
+    getCountyData,
+    successCallback,
+    failureCallback
 }
--- getData/run.js
+++ getData/run.js
@@ -1,11 +1,8 @@
-const { getCountyData } = require('./getData.js');
+const { getCountyData, successCallback,failureCallback } = require('./getData.js');
 console.log('start')
-try {
-    getCountyData('CA', 'https://en.wikipedia.org/wiki/2020_coronavirus_pandemic_in_California', '.sortable tbody tr[class!=sortbottom]', 1,3,5);
-    getCountyData('NY', 'https://en.wikipedia.org/wiki/2020_coronavirus_pandemic_in_New_York_(state)', '.sortable tbody tr[class!=sortbottom]', 1,3,5);
-    getCountyData('WA','https://en.wikipedia.org/wiki/2020_coronavirus_pandemic_in_Washington_(state)', '.sortable tbody tr[class!=sortbottom]', 1,3,5);
-    getCountyData('NJ','https://en.wikipedia.org/wiki/2020_coronavirus_pandemic_in_New_Jersey', '.sortable tbody tr[class!=sortbottom]', 1,3,5);
-    console.log('end')
-} catch(err) {
-    console.log(err)
-}
+getCountyData('CA', 'https://en.wikipedia.org/wiki/2020_coronavirus_pandemic_in_California', '.sortable tbody tr[class!=sortbottom]', 1,3,5).then(successCallback, failureCallback);
+getCountyData('NY', 'https://en.wikipedia.org/wiki/2020_coronavirus_pandemic_in_New_York_(state)', '.sortable tbody tr[class!=sortbottom]', 1,3,5).then(successCallback, failureCallback);
+getCountyData('WA','https://en.wikipedia.org/wiki/2020_coronavirus_pandemic_in_Washington_(state)', '.sortable tbody tr[class!=sortbottom]', 1,3,5).then(successCallback, failureCallback);
+
+
+console.log('all end')
